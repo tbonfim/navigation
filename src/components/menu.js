88191b0be94ce1e1ef0a3a7d6f3b8d0a051c7b5e
Thiago Bonfim <thiago.bonfim@macys.com>
2019-02-06 18:04:26 -0200

    Add slide animation

--- src/components/menu.js
+++ src/components/menu.js
@@ -22,21 +22,47 @@ class Menu extends Component {
   }
   setActiveItem = (element) => {
     const activeClass = 'active';
-    const currentActive = document.getElementsByClassName(activeClass);
-    if ( currentActive.length > 0 ){
-      currentActive[0].classList.remove(activeClass);
+    if (element.currentTarget.classList.contains(activeClass)) {
+      return;
+    }
+    const currentActiveList = document.getElementsByClassName(activeClass);
+    if ( currentActiveList.length > 0 ){
+      currentActiveList[0].classList.remove(activeClass);
     }
     element.currentTarget.classList.add(activeClass);
+    this.goTo(element, currentActiveList[0]);
+  }
+  goTo = (element, currentActive) => {
+
+    // const currentAnimationX = currentActive.getBoundingClientRect().left|| 0;
+    // const currentAnimationWidth = currentActive.offsetWidth || 0;
+    const newAnimationX = element.currentTarget.getBoundingClientRect().left;
+    const newAnimationWidth = element.currentTarget.offsetWidth;
+
+    
+    document.getElementById('slider').style.width  = newAnimationWidth + 'px';
+    document.getElementById('slider').style.transform = 'translateX(' + newAnimationX + 'px)';
+     
+    // document.getElementById('slider').animate([
+    //   // keyframes
+    //   { left: currentAnimationX + 'px', width: currentAnimationWidth + 'px' }, 
+    //   { left: newAnimationX + 'px' , width: newAnimationWidth + 'px' }
+    // ], { 
+    //   duration: 1000
+    // });
   }
   render() {
     let {items} = this.state;
     if(items.length <= 0) return null;
     return (
-      <nav>
-        <span>
-          {items.cities.map( item => ( <a href="#" key={item.section} onClick={this.setActiveItem}>{item.label}</a>) )}
-        </span>
-      </nav>
+      <div>
+        <nav>
+          {items.cities.map( item => ( <a href="#" key={item.section} id={item.section} onClick={this.setActiveItem}>{item.label}</a>) )}
+        </nav>
+        <div id="navigationBar">
+          <span id="slider"></span>
+        </div>
+      </div>
     );
   }
 
