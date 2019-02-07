21c68a7827d79448f9e35cc4f6ea8f8b2dbe336c
Thiago Bonfim <thiago.bonfim@macys.com>
2019-02-07 15:49:19 -0200

    Add current time

--- src/components/menu.js
+++ src/components/menu.js
@@ -9,50 +9,76 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [], 
+      currentTime: null
     };
   }
 
   componentDidMount() {
     this.fetchData();
+    window.addEventListener('resize', this.updateSlider);
+  }
+  updateSlider = () => {
+    const currentActiveElement = document.getElementsByClassName('active')[0];
+    this.goTo(currentActiveElement);
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.updateSlider);
+  }
+  
+  componentWillMount = () => {
+    this.updateSlider();
   }
   
   fetchData = () => {
     this.setState({items : data});
   }
+
   setActiveItem = (element) => {
     const activeClass = 'active';
-    if (element.currentTarget.classList.contains(activeClass)) {
-      return;
-    }
+    if (element.currentTarget.classList.contains(activeClass)) return null;
+
     const currentActiveList = document.getElementsByClassName(activeClass);
     if ( currentActiveList.length > 0 ){
       currentActiveList[0].classList.remove(activeClass);
     }
     element.currentTarget.classList.add(activeClass);
-    this.goTo(element, currentActiveList[0]);
+    this.goTo(element.currentTarget);
+    this.getCurrentTime(element);
   }
-  goTo = (element, currentActive) => {
 
-    // const currentAnimationX = currentActive.getBoundingClientRect().left|| 0;
-    // const currentAnimationWidth = currentActive.offsetWidth || 0;
-    const newAnimationX = element.currentTarget.getBoundingClientRect().left;
-    const newAnimationWidth = element.currentTarget.offsetWidth;
-
+  goTo = (element) => {
+    if(! element) return null;
+    const newAnimationX = element.getBoundingClientRect().left;
+    const newAnimationWidth = element.offsetWidth;
+    const slider = document.getElementById('slider');
     
-    document.getElementById('slider').style.width  = newAnimationWidth + 'px';
-    document.getElementById('slider').style.transform = 'translateX(' + newAnimationX + 'px)';
-     
-    // document.getElementById('slider').animate([
-    //   // keyframes
-    //   { left: currentAnimationX + 'px', width: currentAnimationWidth + 'px' }, 
-    //   { left: newAnimationX + 'px' , width: newAnimationWidth + 'px' }
-    // ], { 
-    //   duration: 1000
-    // });
+    slider.style.width  = newAnimationWidth + 'px';
+    slider.style.transform = 'translateX(' + newAnimationX + 'px)';
+  }
+  getCurrentTime = () => {
+    const currentActiveElement = document.getElementsByClassName('active')[0];
+    if (!currentActiveElement) return null;
+    
+    const citiesTimezones = {
+      'cupertino' : -8,
+      'new-york-city' : -5,
+      'london' : 0,
+      'amsterdam' : 1,
+      'tokyo' : 9,
+      'hong-kong' : 8,
+      'sydney' : 11
+    };
+    const offset = citiesTimezones[currentActiveElement.id];
+    const date = new Date();
+    const utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+    const newDate = new Date(utc + (3600000 * offset));
+  
+    this.setState({currentTime: newDate.getHours() + ':' + newDate.getMinutes()}); 
   }
   render() {
-    let {items} = this.state;
+    const {items} = this.state;
     if(items.length <= 0) return null;
     return (
       <div>
@@ -61,6 +87,10 @@ class Menu extends Component {
         </nav>
         <div id="navigationBar">
           <span id="slider"></span>
+        </div>
+        <div id="currentTime">
+          <h3>{this.state.currentTime ? 'Current Time:': ''}</h3>
+          {this.state.currentTime}
         </div>
       </div>
     );
